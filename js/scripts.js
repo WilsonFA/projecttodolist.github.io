51bd3381f90b487855919294a9c9a1d89fc5281b
Wilson Fernandes <uilsucontato@gmail.com>
2022-12-08 20:17:07 -0300

    feature: add js modify

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldInputValue;
+
 // Funções
 const saveTodo = (text) => {
 
@@ -36,13 +38,80 @@ const saveTodo = (text) => {
     TodoInput.focus();
 }
 
+const toggleForms = () => {
+    editForm.classList.toggle("hide");
+    TodoForm.classList.toggle("hide");
+    TodoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo) => {
+
+        let todoTitle = todo.querySelector("h3");
+
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+        }
+    });
+
+};
+
 // Eventos
 TodoForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const inputValue = TodoInput.value;
 
-    if(inputValue) {
+    if (inputValue) {
         saveTodo(inputValue);
     }
+});
+
+document.addEventListener("click", (e) => {
+
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+    let todoTitle;
+
+    if (parentEl && parentEl.querySelector("h3")) {
+        let todoTitle = parentEl.querySelector("h3").innerText;
+    }
+
+    if (targetEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done");
+    }
+
+    if (targetEl.classList.contains("remove-todo")) {
+        parentEl.remove();
+    }
+
+    if (targetEl.classList.contains("edit-todo")) {
+        toggleForms();
+
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
+    }
+});
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const editInputValue = editInput.value;
+
+    if (editInputValue) {
+        updateTodo(editInputValue);
+    }
+
+    toggleForms();
+
 });
